07d73cd7c33885508506b68542f8cb56ab7e43ca
Florin0216 <florin.barbu16@yahoo.com>
2025-10-23 11:13:51 +0300

    Added boilerplate structure for the project.

---
+++ assets/js/fosJsRouting.js
@@ -0,0 +1,10 @@
+import FosJsRouting from 'fos-router/js/router';
+
+
+import routes from '../static/fos_js_routes.json';
+
+
+FosJsRouting.setRoutingData(routes);
+
+
+export default FosJsRouting;
---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
+
+}
